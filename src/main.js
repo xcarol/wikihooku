50586e36ec5c8b80fd1163c1e5cb11ca8a8b6213
Xavier Carol <xcarolr@gmail.com>
2023-06-01 19:45:35 +0200

    add back vuetify

--- src/main.js
+++ src/main.js
@@ -5,12 +5,13 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 // import store from './store/config';
-// import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify';
 import i18n from './i18n';
 
 const app = createApp(App);
 
 app.use(router);
+app.use(vuetify);
 app.use(i18n);
 
 app.mount('#app');

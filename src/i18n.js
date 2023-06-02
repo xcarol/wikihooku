722bc4503c3d0a9a3e6f2fb1d5904577de1abaf4
Xavier Carol <xcarolr@gmail.com>
2023-06-02 12:33:04 +0200

    clean up

--- src/i18n.js
+++ src/i18n.js
@@ -1,23 +1,11 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { createI18n } from 'vue-i18n';
 // import store from './store/config';
 import ca from './locales/ca.json';
 import en from './locales/en.json';
 import es from './locales/es.json';
 
-// function loadLocaleMessages() {
-//   const localeFiles = require.context('./locales', false, /[A-Za-z0-9-_,\s]+\.json$/i);
-//   const messages = {};
-//   const locales = localeFiles.keys().map((localeFile) => [localeFile, localeFile.match(/([A-Za-z0-9-_]+)\./i).pop()]);
-//   locales.forEach((locale) => {
-//     const [file, key] = locale;
-//     // eslint-disable-next-line security/detect-object-injection
-//     messages[key] = localeFiles(file);
-//   });
-//   return messages;
-// }
-
 const messages = { ca, en, es };
-// const messagesKeys = Object.keys(messages);
 
 function getUserLanguage() {
   return 'es';
@@ -44,18 +32,3 @@ const i18n = createI18n({
 });
 
 export default i18n;
-
-// import { localize, ValidationProvider } from 'vee-validate';
-// import localeCa from 'vee-validate/dist/locale/ca.json';
-// import localeEn from 'vee-validate/dist/locale/en.json';
-// import localeEs from 'vee-validate/dist/locale/es.json';
-
-// localize({
-//   ca: { code: localeCa.code, messages: { ...messages.ca.messages, ...localeCa.messages }, names: { ...messages.ca.fields } },
-//   es: { code: localeEs.code, messages: { ...messages.es.messages, ...localeEs.messages }, names: { ...messages.es.fields } },
-//   en: { code: localeEn.code, messages: { ...messages.en.messages, ...localeEn.messages }, names: { ...messages.en.fields } },
-// });
-
-// localize(locale);
-
-// Vue.component('ValidationProvider', ValidationProvider);

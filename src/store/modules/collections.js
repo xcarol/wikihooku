332476931f781fc04ab3e81b799b1a362594e44d
Xavier Carol <xcarolr@gmail.com>
2023-09-05 19:15:57 +0200

    add collections to store

--- src/store/modules/collections.js
+++ src/store/modules/collections.js
@@ -1,3 +1,7 @@
+import ApiPlugin from '../../plugins/api';
+
+const { api } = ApiPlugin;
+
 const getters = {
 };
 
@@ -5,6 +9,12 @@ const mutations = {
 };
 
 const actions = {
+  save({ commit }, collectionInfo) {
+    return api.saveCollection(collectionInfo.userId, collectionInfo.name, collectionInfo.items)
+    .then((res) => {
+      commit('viewTitle', res.data.collection.name, { root: true });
+    });
+  }
 };
 
 const state = {

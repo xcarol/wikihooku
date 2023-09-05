29de34643ba49d1e310382d36ef11d1f10e121db
Xavier Carol <xcarolr@gmail.com>
2023-09-05 19:13:13 +0200

    add view title at application tool bar

--- src/components/toolbar/AppToolbar.vue
+++ src/components/toolbar/AppToolbar.vue
@@ -5,6 +5,8 @@
     <home-button @home="home" />
     <feedback-button @feedback="feedback" />
     <v-spacer />
+    <p class="text-button font-weight-bold">{{ title }}</p>
+    <v-spacer />
     <app-toolbar-menu
       @login="login"
       @logout="logout"
@@ -14,6 +16,9 @@
 </template>
 
 <script setup>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+
 import HomeButton from './HomeButton.vue';
 import FeedbackButton from './FeedbackButton.vue';
 import AppToolbarMenu from './AppToolbarMenu.vue'
@@ -24,4 +29,8 @@ const home = () => emits('home');
 const login = () => emits('login');
 const logout = () => emits('logout');
 const register = () => emits('register');
+
+const store = useStore();
+
+const title = computed(() => store.getters.viewTitle);
 </script>

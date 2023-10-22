4545ef23e764329d9c8448953fb26ff3693adfc4
Xavier Carol <xcarolr@gmail.com>
2023-10-22 18:23:07 +0200

    show services logos

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vuejs-accessibility/anchor-has-content -->
 <template>
   <v-footer
     class="pa-0 flex-grow-0 flex-shrink-0"
@@ -5,23 +6,41 @@
   >
     <v-row no-gutters>
       <v-col
-        class="text-left ma-4 me-auto"
+        class="text-left ma-4 me-auto align-center"
         cols="12"
         sm="auto"
       >
-        <span>
-          {{ $t('global.madewith') }}
-          <v-icon size="small">
-            <v-img
-              :src="logoImg"
-              width="100%"
-              height="100%"
-              class="logo"
-              :inline="true"
-              @click="goto('home')"
-            />
-          </v-icon>
-        </span>
+        <v-row no-gutters>
+          <v-col
+            cols="12"
+            sm="auto"
+          >
+            {{ $t('global.madewith') }}
+          </v-col>
+          <v-col
+            v-for="(logo, index) in logos"
+            :key="index"
+            cols="12"
+            sm="auto"
+            class="ml-2"
+          >
+            <v-tooltip
+              activator="parent"
+              location="top"
+              >{{ logo.name }}</v-tooltip
+            >
+            <a
+              :href="logo.url"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <v-img
+                :src="logo.img"
+                class="v-icon"
+              />
+            </a>
+          </v-col>
+        </v-row>
       </v-col>
       <v-col
         class="text-center ma-4"
@@ -45,9 +64,45 @@
 
 <script setup>
 import LanguageSelector from './LanguageSelector.vue';
+import GithubIcon from '../assets/github.png';
+import MongodbIcon from '../assets/mongodb.ico';
+import RenderIcon from '../assets/render.svg';
+import SendgridIcon from '../assets/sendgrid.png';
 import VueIcon from '../assets/vue.svg';
+import VuetifyIcon from '../assets/vuetify.svg';
 
-const logoImg = VueIcon;
+const logos = [
+  {
+    img: VueIcon,
+    url: 'https://vuejs.org/',
+    name: 'Vue',
+  },
+  {
+    img: VuetifyIcon,
+    url: 'https://vuetifyjs.com/',
+    name: 'Vuetify',
+  },
+  {
+    img: GithubIcon,
+    url: 'https://github.com/xcarol/wikihooku/',
+    name: 'GitHub',
+  },
+  {
+    img: MongodbIcon,
+    url: 'https://www.mongodb.com/',
+    name: 'MongoDb',
+  },
+  {
+    img: RenderIcon,
+    url: 'https://render.com/',
+    name: 'Render',
+  },
+  {
+    img: SendgridIcon,
+    url: 'https://sendgrid.com/',
+    name: 'SendGrid',
+  },
+];
 </script>
 
 <style scoped>

ee0d2050b4aad59a3b0f2b3b142f7d0a6c011d6a
Xavier Carol <xcarolr@gmail.com>
2023-10-31 19:12:09 +0100

    Feature/41 prepare to publish (#44)
    * added cookies support
    
    * move i18n support to plugins folder
    
    * add footer with
    - language selector
    - motto
    - Terms of service (link)
    
    * set default collection name
    
    * set user locale after login
    
    * remove foter messages
    moved to global
    
    * update translation names, also
    update route for terms of service
    
    * add terms of service route
    
    * add terms of service view
    
    * rename literals
    
    * add terms of service
    
    * add services logos
    
    * show services logos
    
    * update terms texts
    
    * remove obsolete logos
    
    * remove uneeded manifest.json file
    
    * add meta description tag
    
    * add meta keywords tag
    
    * remove variant attribute from buttons
    
    * remove sendgrid from footer
    
    * remove language duplicated literals
    
    * update version

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -20,7 +20,7 @@
           <v-col
             v-for="(logo, index) in logos"
             :key="index"
-            cols="12"
+            cols="1"
             sm="auto"
             class="ml-2"
           >
@@ -67,7 +67,6 @@ import LanguageSelector from './LanguageSelector.vue';
 import GithubIcon from '../assets/github.png';
 import MongodbIcon from '../assets/mongodb.ico';
 import RenderIcon from '../assets/render.svg';
-import SendgridIcon from '../assets/sendgrid.png';
 import VueIcon from '../assets/vue.svg';
 import VuetifyIcon from '../assets/vuetify.svg';
 
@@ -96,11 +95,6 @@ const logos = [
     img: RenderIcon,
     url: 'https://render.com/',
     name: 'Render',
-  },
-  {
-    img: SendgridIcon,
-    url: 'https://sendgrid.com/',
-    name: 'SendGrid',
   },
 ];
 </script>
